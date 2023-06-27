1af8b125a0dd31330fd93dc2ddb1e2f09f838398
Alessio Argentati <2006alessio@gmail.com>
2023-06-27 16:52:32 +0200

    Kommentare eingefügt, Error Frame und Design

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-con = sqlite3.connect("passvault.db")
-cur = con.cursor()
+con = sqlite3.connect("passvault.db") #Erstellen einer Verbindung zur Datenbank. Besser gesagt dem File
+cur = con.cursor() #Erstellt ein Cursor
 
-
+#Funktion für zum erhalten eines User nach einem Namen
 def select_user(username):
     cur.execute("SELECT * FROM Users WHERE username = ?", (username,))
     rows = cur.fetchall()
@@ -11,7 +11,8 @@ def select_user(username):
         return False
     else:
         return rows[0]
-    
+
+#Erstellt ein User, gibt den Wert "unique" zurück wenn der User schon existiert.    
 def create_user(username, password):
     try:
         cur.execute("SELECT * FROM Users WHERE username = ?", (username,))
@@ -25,7 +26,7 @@ def create_user(username, password):
     except:
         return False
 
-    
+#Funktion zum erhalten der Logins von einem User    
 def select_logins_by_user(username):
     cur.execute("SELECT * FROM Logins JOIN Users ON User_ID = ID_User WHERE Users.username = ?", (username,))
     rows = cur.fetchall()
@@ -34,14 +35,18 @@ def select_logins_by_user(username):
     else:
         return rows
 
-def select_login(id):
+#CRUD von den Logins. Create, Read, Update, Delete
+
+#Funktion zum erhalten eines Logins
+def select_login(id):   
     cur.execute("SELECT * FROM Logins WHERE ID_Login = ?", (id,))
     rows = cur.fetchall()
     if len(rows)==0:
         return False
     else:
         return rows[0]
-    
+
+#Funktion zum erstellen eines Logins
 def create_login(plattform, username, password, user_id):
     try:
         cur.execute("INSERT INTO Logins(plattform, username, password, User_ID) VALUES (?,?,?,?)", (plattform, username, password, user_id))
@@ -49,7 +54,8 @@ def create_login(plattform, username, password, user_id):
         return cur.lastrowid
     except:
         return False
-    
+
+#Funktion um ein Login zu löschen
 def delete_login(id):
     try:
         cur.execute("DELETE FROM Logins WHERE ID_Login = ?", (id,))
@@ -57,7 +63,8 @@ def delete_login(id):
         return True
     except:
         return False
-    
+
+#Funktion fpr zum bearbeiten eines Logins
 def update_login(id, plattform, username, password):
     try:
         cur.execute("UPDATE Logins SET plattform = ?, username = ?, password = ? WHERE ID_Login = ?", (plattform, username, password, id))
--- gui.py
+++ gui.py
@@ -1,90 +1,127 @@
 import customtkinter
 import re
-import database
+import database #Importiert die database Datei. Dient wie ein DAO (Data Access Object)
 
+#Einzelne Frames für die verschiedenen Komponente der App, die Global zugegrifen müssen werden
 root = customtkinter.CTk()
 loginFrame = customtkinter.CTkFrame(master=root)
 registerFrame = customtkinter.CTkFrame(master=root)
 mainFrame = customtkinter.CTkFrame(master=root)
 informationFrame = customtkinter.CTkFrame(master=mainFrame, width=550, height=500)
 
-loggedInUsername = ""
-loggedInUserId = None
+#Globale variablen
+loggedInUsername = None #Sobald login, den aktuellen Benutzername
+loggedInUserId = None #Sobald login, die akutelle ID vom User (Datenbank)
+alreadyError = False  #Verhindert erneutes öffnen von Fehlerfenstern
 
+#Funktion für zum starten des Programms. Wird über main aufgerufen
 def init():
-    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_appearance_mode("system")
     customtkinter.set_default_color_theme("green")
-    root.geometry("700x500")
+    root.geometry("700x500") #Grösse Fenster
     root.title("PassVault")
     root.resizable(False, False)
     root.iconbitmap("logo.ico")
-    set_login()
+    set_login() #Erstellt zu begin des Programm das login Fenster
     root.mainloop()
 
+#Erstellt ein Error Fenster mit der mitgegebenen Nachricht
+def error_frame(error):
+    global alreadyError
+    if (not (alreadyError == True)):
+        alreadyError = True
+        errorFrame = customtkinter.CTkToplevel()
+        errorFrame.title("Error")
+        errorFrame.resizable(False, False)
+        errorMessage = customtkinter.CTkLabel(master=errorFrame, text=error)
+        errorMessage.pack(pady=10, padx=10)
+        def close_error():
+            errorFrame.destroy()
+            global alreadyError
+            alreadyError = False
+        closeButton = customtkinter.CTkButton(master=errorFrame, text="Close", command= lambda : close_error())
+        closeButton.pack(pady=10, padx=10)
+        errorFrame.wm_transient(root) #Verhindert das das Error Fenster hinter dem Hauptfenster erstellt wird
+
+#Überprüft ob das Passwort "sicher" ist.
+def secure_password(password):
+    x = re.search("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", password)
+    # (?=.*\d) -> Checkt ob mindestens eine Zahl vorhanden ist
+    # (?=.*[a-z]) -> Checkt ob es mindestens ein kleiner Buchstaben hat
+    # (?=.*[A-Z]) -> Checkt ob es mindestens ein grossen Buchstaben hat
+    # (?=.*[a-zA-Z]) -> Checkt ob es überhaupt ein Zeichen hat
+    # {8,} -> Mindestens 8 Zeichen
+    return x
+
+#Überprüft ob das Password mit dem eingegebenen Übereinstimmt. Checkt auch ob es diesen Überhaupt gibt.
 def check_login(username, password):
     userInfo = database.select_user(username)
     if(not userInfo):
-        print("Not found")
+        error_frame("User doesn't exit.")
     elif (userInfo[2] == password):
         global loggedInUsername
         loggedInUsername = username
         global loggedInUserId
         loggedInUserId = userInfo[0]
-        set_main()
+        set_main() #Erstellt das eingeloggte GUI
     else:
-        print("Password wrong")
+        error_frame("Wrong password.")
 
+#Erstellt ein User und erkennt alle möglichen Fehler
 def create_user(username, password, repassword):
     if (not (username == "")):
         if (not (password == "" or repassword == "")):
             if (password == repassword):
-                x = re.search("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", password)
-                if (not (x == None)):
+                if (not (secure_password(password) == None)):
                     feedbackCreate = database.create_user(username, password)
                     print(feedbackCreate)
                     if (not feedbackCreate):
-                        print("error: couldn't create user")
+                        error_frame("error: couldn't create user")
                     elif (feedbackCreate == "unique"):
-                        print("User already exists")
+                        error_frame("User already exists.")
                     else:
-                        registerFrame.pack_forget()
-                        set_login()
+                        registerFrame.pack_forget() 
+                        set_login() #Leitet den User nach einem erfolgreichem regristrieren zur Login Seite
                 else:
-                    print("The password needs atleast 8 letter. It must contain a small letter, a capital letter, and a number.")
+                    error_frame("The password needs atleast 8 letter. It must contain a small letter, a capital letter, and a number.")
             else:
-                print("The passwords don't match")
+                error_frame("The passwords don't match.")
         else:
-            print("Please enter a password")
+            error_frame("Please enter a password.")
     else:
-        print("Please enter a username")
+        error_frame("Please enter a username.")
 
        
-            
+#Erstellt ein neues Login für einen bestimmten User.            
 def create_login(plattform, username, password, user_id):
     feedbackCreate = database.create_login(plattform, username, password, user_id)
     if (not feedbackCreate):
-       print("error: couldn't create login")
+       error_frame("error: couldn't create login")
     else:
-        set_main()
-        set_login_frame(feedbackCreate)
+        set_main() #Erfrischt die gnaze Seite für das der neue Eintrag sichtbar ist
+        set_login_frame(feedbackCreate) #Zeigt direkt den Eintrag an, wenn dieser erfolgreich erstellt wurde
 
+#Löscht ein Login
 def delete_login(id):
     feedbackDelete = database.delete_login(id)
     if (not feedbackDelete):
-        print("error: couldn't delete login")
+        error_frame("error: couldn't delete login")
     else:
+        #Zerstört alle Komponenten auf dem Informations Frame
         for child in informationFrame.winfo_children():
             child.destroy()
-        set_main()
+        set_main() #Refreshed die ganze Seite für das der Eintrag nicht mehr sichtbar ist.
+
+#Bearbeitet ein Login
 def edit_login(id, plattform, username, password):
     feedbackEdit = database.update_login(id, plattform, username, password)
     if (not feedbackEdit):
-        print("error: couldn't update login")
+        error_frame("error: couldn't update login")
     else: 
-        set_main()
-        set_login_frame(id)
+        set_main() #Refreshed die Seite falls es eine Änderung am Titel gäbe
+        set_login_frame(id) #Zeigt den neuen bearbeiteten Eintrag direkt an
         
-
+#Switched zwischen dem Regristrieren und dem Login Frame
 def switch_login_register(switch):
     if switch == True:
         loginFrame.pack_forget()
@@ -93,52 +130,64 @@ def switch_login_register(switch):
         registerFrame.pack_forget()
         set_login()
 
+#Zeigt das den Informations Frame an. Ähnlich wie wenn man den Frame nun sichtbar macht
 def grid_information():
     informationFrame.pack_propagate(0)
     informationFrame.grid(pady=10, padx=(0,10),row=0, rowspan=3, column=1, columnspan=5, sticky='w')
 
+#Erstellt das "Bearbeiten von einem Login" Frame.
 def set_edit_login_frame(id):
     login = database.select_login(id)
     if (not login):
-        print("error: id doesnn't match with database entry")
+        error_frame("error: id doesn't match with database entry")
     else:
         informationFrame.grid_remove()
         for child in informationFrame.winfo_children():
             child.destroy()
         plattformEditInput = customtkinter.CTkEntry(master=informationFrame, placeholder_text="Plattform Name")
-        plattformEditInput.insert(0, login[1])
+        plattformEditInput.insert(0, login[1]) #Fügt in das Eingabefeld die jetzigen Daten ein
         plattformEditInput.pack(pady=10, padx=10)
         usernameEditInput = customtkinter.CTkEntry(master=informationFrame, placeholder_text="Username")
-        usernameEditInput.insert(0, login[2])
+        usernameEditInput.insert(0, login[2]) #Fügt in das Eingabefeld die jetzigen Daten ein
         usernameEditInput.pack(pady=10, padx=10)
         passwordEditInput = customtkinter.CTkEntry(master=informationFrame, placeholder_text="Password")
-        passwordEditInput.insert(0, login[3])
+        passwordEditInput.insert(0, login[3]) #Fügt in das Eingabefeld die jetzigen Daten ein
         passwordEditInput.pack(pady=10, padx=10)
         editButton = customtkinter.CTkButton(master=informationFrame, text="Edit", command= lambda : edit_login(id, plattformEditInput.get(), usernameEditInput.get(), passwordEditInput.get()))
         editButton.pack(pady=10, padx=10)
         grid_information() 
         
-
+#Erstellt den Informations Frame für ein einzelnes Login
 def set_login_frame(id):
     login = database.select_login(id)
     if (not login):
-        print("error: id doesn't match with database entry")
+        error_frame("error: id doesn't match with database entry")
     else:
         informationFrame.grid_remove()
         for child in informationFrame.winfo_children():
             child.destroy()
-        plattform = customtkinter.CTkLabel(master=informationFrame, text=login[1])
-        plattform.pack(pady=10,padx=10)
-        username = customtkinter.CTkLabel(master=informationFrame, text=login[2])
+        plattform = customtkinter.CTkLabel(master=informationFrame, text=login[1] + " Login", font=("Roboto", 36))
+        plattform.pack(pady=(10, 30),padx=10)
+        username = customtkinter.CTkLabel(master=informationFrame, text="Username: "+login[2])
         username.pack(pady=10,padx=10)
-        password = customtkinter.CTkLabel(master=informationFrame, text=login[3])
+        password = customtkinter.CTkLabel(master=informationFrame, text="Password: "+login[3])
         password.pack(pady=10, padx=10)
+        passwordSecureOrNot = None
+        if (secure_password(login[3]) == None):
+            passwordSecureOrNot = "Your password is not secure."
+        else:
+            passwordSecureOrNot = "Your password is secure."
+        passwordSecure = customtkinter.CTkLabel(master=informationFrame, text=passwordSecureOrNot)
+        passwordSecure.pack(pady=10, padx=10)
+        passwordSecureInformation = customtkinter.CTkLabel(master=informationFrame, text="We check if the password has atleast 8 smybols, a lowercase letter, an uppercase letter, and a number.", font=("Roboto", 11))
+        passwordSecureInformation.pack(pady=10,padx=10)
         editButton = customtkinter.CTkButton(master=informationFrame, text="Edit Login", command=lambda : set_edit_login_frame(id))
         editButton.pack(pady=10, padx=10)
         deleteButton = customtkinter.CTkButton(master=informationFrame, text="Delete Login", command=lambda : delete_login(id))
         deleteButton.pack(pady=10, padx=10)
         grid_information()
 
+#Erstellt das Frame für zum erstellen von einem Login
 def set_create_login_frame():
     informationFrame.grid_remove()
     for child in informationFrame.winfo_children():
@@ -153,6 +202,20 @@ def set_create_login_frame():
     createButton.pack(pady=10, padx=10)
     grid_information()
 
+#Fügt je nach dem ob man schon Einträge erstellt hat ein anderes Anfangs Frame ein.
+def set_start_frame(alreadyEntries):
+    if(alreadyEntries):
+        description = "Current Version 0.1\nMade with love by Alessio and Jeremias"    
+    else:
+        description = 'You currently have no logins. \nStart creating your own logins by pressing on the "Create Login" button.'
+    informationFrame.grid_remove()
+    startTitle = customtkinter.CTkLabel(master=informationFrame, text="Welcome to PassVault, " + loggedInUsername + ".", font=("Roboto", 36))
+    startTitle.pack(pady=(10,30), padx=10)
+    description = customtkinter.CTkLabel(master=informationFrame, text=description)
+    description.pack(pady=10, padx=10)
+    grid_information()
+
+#Erstellt das einloggen Frame
 def set_login():
     loginFrame.pack(pady=90, padx=210, fill="both", expand=True)
 
@@ -171,8 +234,9 @@ def set_login():
     link = customtkinter.CTkFont(family="Roboto", size=12, underline=True)
     register = customtkinter.CTkLabel(master=loginFrame, text="Don't have an account? Sign Up", font=link, cursor="hand2")
     register.pack(pady=12,padx=10)
-    register.bind("<Button-1>", lambda e: switch_login_register(True))
+    register.bind("<Button-1>", lambda e: switch_login_register(True)) #Bindet das Button-1 (Klick) Event an.
 
+#Erstellt das registrieren Frame
 def set_register():
     registerFrame.pack(pady=90, padx=210, fill="both", expand=True)
 
@@ -194,14 +258,15 @@ def set_register():
     link = customtkinter.CTkFont(family="Roboto", size=12, underline=True)
     register = customtkinter.CTkLabel(master=registerFrame, text="Already have an account? Login", font=link, cursor="hand2")
     register.pack(pady=(4,12),padx=10)
-    register.bind("<Button-1>", lambda e: switch_login_register(False)) 
+    register.bind("<Button-1>", lambda e: switch_login_register(False)) #Bindet das Button-1 (Klick) Event an.
 
+#Erstellt das Hauptframe nach einem erfolgreichem Login
 def set_main():
     loginFrame.pack_forget()
     registerFrame.pack_forget()
-    mainFrame.columnconfigure(0, weight=1)
-    mainFrame.columnconfigure(1, weight=1)
-    mainFrame.rowconfigure(1, weight=1)
+    mainFrame.columnconfigure(0, weight=1) #Einstellung für ein Raster Layout
+    mainFrame.columnconfigure(1, weight=1) #  " "
+    mainFrame.rowconfigure(1, weight=1) #     " "
     mainFrame.pack(pady=5, padx = 5, fill="both", expand=True)
 
     listFrame = customtkinter.CTkScrollableFrame(master=mainFrame, width=100, height=400)
@@ -212,17 +277,19 @@ def set_main():
 
     grid_information()
 
+    #Hohlt alle akutellen Logins und zeigt diese am Rand an
     userInfo = database.select_logins_by_user(loggedInUsername)
     if (not userInfo):
-        print("No entries")
-    else: 
+        set_start_frame(False)
+    else:
+        set_start_frame(True) 
         for i in range(len(userInfo)):
             frame = customtkinter.CTkFrame(master=listFrame)
             label = customtkinter.CTkLabel(master=frame, text=(userInfo[i])[1], font=("Roboto", 16), cursor="hand2")
             label.pack(pady=1,padx=1, fill="x")
             def make_lambda(x):
                 return lambda e:set_login_frame(x)
-            label.bind("<Button-1>", make_lambda(((userInfo[i])[0])))
+            label.bind("<Button-1>", make_lambda(((userInfo[i])[0]))) #Bindet das Klick Event auf das Frame
             frame.pack(pady=2, padx=2, fill="x")
             
 
